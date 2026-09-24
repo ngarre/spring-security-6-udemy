bff7a20919f9f1a5f6928cf8d8610e789093d03c
Garré Ramo <nigo@gft.com>
2026-09-24 15:59:15 +0200

    fix: nombre directorio

--- src/main/java/com/cursospringsecurity/app_security/security/SecurityConfig.java
+++ src/main/java/com/cursospringsecurity/app_security/security/SecurityConfig.java
@@ -1,6 +1,5 @@
-package com.cursospringsecurity.app_security.ssecurity;
+package com.cursospringsecurity.app_security.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
